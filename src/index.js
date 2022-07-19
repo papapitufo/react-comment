0f1b158c1cba9941f408aa23124391f83187e2eb
Roberto Moller <rmoller@sitetracker.com>
2022-07-19 16:28:43 -0400

    intial commit, using lean-react-starter for bootstraping

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './components/App';
+
+ReactDom.createRoot(document.querySelector(".container")).render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: "development",
+  module: {
+      rules: [
+          {
+              loader: "babel-loader"
+          }
+      ]
+    }
+}
